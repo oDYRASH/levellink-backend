bd54c1b06be0601d80619f98c43677e03b099f29
Tony <64509934+oDYRASH@users.noreply.github.com>
2024-02-27 23:14:28 +0100

    host settings

--- config.py
+++ config.py
@@ -1,8 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-
-app = Flask(__name__)
+app = Flask("app")
 CORS(app)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mydatabase.db"
