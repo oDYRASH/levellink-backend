6823d264e86632119795b0a159c5f216eb3b8725
Tony <64509934+oDYRASH@users.noreply.github.com>
2024-03-03 20:25:15 +0100

    csrf auth

---
+++ auth.py
@@ -0,0 +1,67 @@
+from flask import request, redirect , make_response
+import secrets
+from functools import wraps
+import settings
+import datetime
+
+
+from config import db 
+# Modèle pour stocker les jetons CSRF
+class CSRFToken(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.BigInteger, unique=True, nullable=False)
+    csrf_token = db.Column(db.String(32), nullable=False)
+
+
+def assign_CSRF_to_USER(user_id) -> str:
+    csrf_token = secrets.token_hex(16)  # Génère un jeton CSRF aléatoire
+
+    # nouveau_token = CSRFToken(user_id=user_id, csrf_token=csrf_token)
+    # db.session.add(nouveau_token)
+    # db.session.commit()
+
+    return csrf_token
+
+
+def verifier_csrf_token_and_userId(user_id, csrf_token):
+    token_enregistre = CSRFToken.query.filter_by(user_id=user_id).first()
+    if token_enregistre:
+        return token_enregistre.csrf_token == csrf_token
+    return False
+
+
+def user_id_from_csrf(csrf_token):
+    token_enregistre = CSRFToken.query.filter_by(csrf_token=csrf_token).first()
+    if token_enregistre:
+        return token_enregistre.user_id
+    return False
+
+
+def csrf_auth_required(f):
+
+    @wraps(f)
+    def decorateur(*args, **kwargs):
+        csrf_token = request.cookies.get('csrf_token')
+
+        #pas de Token
+        if not csrf_token : return redirect(settings.FRONTEND_BASE_ROUTE + "/login")
+         
+        token_enregistre = CSRFToken.query.filter_by(csrf_token=csrf_token).first()
+
+        #bad Token
+        if not token_enregistre : return redirect(settings.FRONTEND_BASE_ROUTE + "/login")    
+
+        return f(*args, **kwargs)
+    
+    return decorateur
+
+def make_csrf_setting_response(csrf_token, live_time_in_days):
+
+    # Définir le cookie CSRF dans la réponse
+    max_age = live_time_in_days*24*60*60  #one year
+    expires = datetime.datetime.strftime(datetime.datetime.utcnow() + datetime.timedelta(seconds=max_age), "%a, %d-%b-%Y %H:%M:%S GMT")
+    
+    response = make_response()
+    response.set_cookie("csrf_token", csrf_token, max_age=max_age, expires=expires)
+
+    return response
