4197e5ed3007897aea8701ea0f66a6d69943e581
Tony <64509934+oDYRASH@users.noreply.github.com>
2024-03-05 21:00:19 +0100

    no dotenv lib

--- settings.py
+++ settings.py
@@ -1,8 +1,8 @@
 import os
 
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
-load_dotenv()
+# load_dotenv()
 SOCIAL_AUTH_DISCORD_KEY = os.getenv("SOCIAL_AUTH_DISCORD_KEY")
 SOCIAL_AUTH_DISCORD_SECRET = os.environ.get("SOCIAL_AUTH_DISCORD_SECRET")
 SOCIAL_AUTH_DISCORD_REDIRECT_URI = os.environ.get("SOCIAL_AUTH_DISCORD_REDIRECT_URI")
