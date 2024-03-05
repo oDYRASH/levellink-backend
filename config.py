8a4546a6628e29cf62187e7dfa9d10132230f0e4
Tony <64509934+oDYRASH@users.noreply.github.com>
2024-03-05 19:49:23 +0100

    CORS settings

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ from flask_cors import CORS
 
 app = Flask("app")
 
-CORS(app, origins=["https://backend.levellink.lol", "https://levellink.lol"])
-
+# CORS(app, origins=["https://backend.levellink.lol", "https://levellink.lol"])
+CORS(app, resources={r"/*": {"origins": "*", "supports_credentials": True}})
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mydatabase.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SECRET_KEY"] = "45a5fc12445dbac2f59c35a5fc12b6"
